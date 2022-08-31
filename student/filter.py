d12675d920a4f97662faf9c0cb9eb52aaefe06f2
Michele <michele.pestarino@outlook.it>
2022-08-31 16:34:10 +0200

    Final project - Step 2 done and working

--- student/filter.py
+++ student/filter.py
@@ -19,7 +19,7 @@ import sys
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-import misc.params as params 
+import misc.params as params
 
 class Filter:
     '''Kalman filter class'''
@@ -27,66 +27,58 @@ class Filter:
         pass
 
     def F(self):
-        ############
-        # TODO Step 1: implement and return system matrix F
-        ############
-
-        return 0
-        
-        ############
-        # END student code
-        ############ 
+        # Step 1.1: implement and return system matrix F
+        return np.matrix([[1, 0, 0, params.dt, 0, 0],
+                        [0, 1, 0, 0, params.dt, 0],
+                        [0, 0, 1, 0, 0, params.dt],
+                        [0, 0, 0, 1, 0, 0],
+                        [0, 0, 0, 0, 1, 0],
+                        [0, 0, 0, 0, 0, 1]])
 
     def Q(self):
-        ############
-        # TODO Step 1: implement and return process noise covariance Q
-        ############
-
-        return 0
-        
-        ############
-        # END student code
-        ############ 
+        # Step 1.2: implement and return process noise covariance Q
+        q1 = ((params.dt**3)/3) * params.q 
+        q2 = ((params.dt**2)/2) * params.q 
+        q3 = params.dt * params.q 
+        return np.matrix([[q1, 0, 0, q2, 0, 0],
+                        [0, q1, 0, 0, q2, 0], 
+                        [0, 0, q1, 0, 0, q2],
+                        [q2, 0, 0, q3, 0, 0],
+                        [0, q2, 0, 0, q3, 0],
+                        [0, 0, q2, 0, 0, q3]])
 
     def predict(self, track):
-        ############
-        # TODO Step 1: predict state x and estimation error covariance P to next timestep, save x and P in track
-        ############
-
-        pass
-        
-        ############
-        # END student code
-        ############ 
+        # Step 1.3: predict state x and estimation error covariance P to next timestep and save x and P in track
+        F = self.F()
+        x = F * track.x # state prediction
+        P = F * track.P * F.transpose() + self.Q() # covariance prediction
+        track.set_x(x)
+        track.set_P(P)
+         
 
     def update(self, track, meas):
-        ############
-        # TODO Step 1: update state x and covariance P with associated measurement, save x and P in track
-        ############
-        
-        ############
-        # END student code
-        ############ 
+        # Step 1.4: update state x and covariance P with associated measurement, save x and P in track
+        gamma = self.gamma(track, meas)
+        print("gamma = ", gamma)
+        H = meas.sensor.get_H(track.x) 
+        S = self.S(track, meas, H)
+        K = track.P * H.transpose() * np.linalg.inv(S) # Kalman gain
+        x = track.x + K * gamma # state update
+        I = np.identity(params.dim_state)
+        P = (I - K * H) * track.P # covariance update
+        track.set_x(x)
+        track.set_P(P)
+
         track.update_attributes(meas)
     
     def gamma(self, track, meas):
-        ############
-        # TODO Step 1: calculate and return residual gamma
-        ############
-
-        return 0
+        # Step 1.5: calculate and return residual gamma
+        H = meas.sensor.get_H(track.x)
         
-        ############
-        # END student code
-        ############ 
+        #z = meas.sensor.get_hx(track.x) # measurement function evaluated at the current state
+        return (meas.z - H * track.x) # residual
 
     def S(self, track, meas, H):
-        ############
-        # TODO Step 1: calculate and return covariance of residual S
-        ############
-
-        return 0
-        
-        ############
-        # END student code
-        ############ 
+        # Step 1.6: calculate and return covariance of residual S
+        S = H * track.P * H.transpose() + meas.R # covariance of residual
+        return S
--- student/trackmanagement.py
+++ student/trackmanagement.py
@@ -28,31 +28,29 @@ class Track:
         print('creating track no.', id)
         M_rot = meas.sensor.sens_to_veh[0:3, 0:3] # rotation matrix from sensor to vehicle coordinates
         
-        ############
-        # TODO Step 2: initialization:
+        # Step 2 - initialization:
         # - replace fixed track initialization values by initialization of x and P based on 
         # unassigned measurement transformed from sensor to vehicle coordinates
-        # - initialize track state and track score with appropriate values
-        ############
-
-        self.x = np.matrix([[49.53980697],
-                        [ 3.41006279],
-                        [ 0.91790581],
-                        [ 0.        ],
-                        [ 0.        ],
-                        [ 0.        ]])
-        self.P = np.matrix([[9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 6.4e-03, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+01]])
-        self.state = 'confirmed'
-        self.score = 0
         
-        ############
-        # END student code
-        ############ 
+        pos_sens = np.ones((4, 1)) # homogeneous coordinates
+        pos_sens[0:3] = meas.z[0:3] 
+        pos_veh = meas.sensor.sens_to_veh * pos_sens
+        self.x = np.zeros((6,1))
+        self.x[0:3] = pos_veh[0:3]
+
+        P_pos = M_rot * meas.R * np.transpose(M_rot)
+        # set up velocity estimation error covariance
+        P_vel = np.matrix([[params.sigma_p44**2, 0, 0],
+                        [0, params.sigma_p55**2, 0],
+                        [0, 0, params.sigma_p66**2]])
+        # overall covariance initialization
+        self.P = np.matrix(np.zeros((6, 6)))
+        self.P[0:3, 0:3] = P_pos
+        self.P[3:6, 3:6] = P_vel
+
+        # - initialize track state and track score with appropriate values
+        self.state = 'initialized'
+        self.score = 1./params.window
                
         # other track attributes
         self.id = id
@@ -79,10 +77,7 @@ class Track:
             self.length = c*meas.length + (1 - c)*self.length
             self.height = c*meas.height + (1 - c)*self.height
             M_rot = meas.sensor.sens_to_veh
-            self.yaw = np.arccos(M_rot[0,0]*np.cos(meas.yaw) + M_rot[0,1]*np.sin(meas.yaw)) # transform rotation from sensor to vehicle coordinates
-        
-        
-###################        
+            self.yaw = np.arccos(M_rot[0,0]*np.cos(meas.yaw) + M_rot[0,1]*np.sin(meas.yaw)) # transform rotation from sensor to vehicle coordinates       
 
 class Trackmanagement:
     '''Track manager with logic for initializing and deleting objects'''
@@ -93,12 +88,10 @@ class Trackmanagement:
         self.result_list = []
         
     def manage_tracks(self, unassigned_tracks, unassigned_meas, meas_list):  
-        ############
-        # TODO Step 2: implement track management:
+        # Step 2: implement track management:
         # - decrease the track score for unassigned tracks
         # - delete tracks if the score is too low or P is too big (check params.py for parameters that might be helpful, but
         # feel free to define your own parameters)
-        ############
         
         # decrease score for unassigned tracks
         for i in unassigned_tracks:
@@ -106,14 +99,10 @@ class Trackmanagement:
             # check visibility    
             if meas_list: # if not empty
                 if meas_list[0].sensor.in_fov(track.x):
-                    # your code goes here
-                    pass 
-
-        # delete old tracks   
-
-        ############
-        # END student code
-        ############ 
+                    track.score -= 0.05
+            # delete old tracks (due the low track score or x,y values above threshold in track.P)
+            if track.score < 0.05 or (track.P[0,0] > params.max_P or track.P[1,1] > params.max_P):
+                self.delete_track(track)
             
         # initialize new track with unassigned measurement
         for j in unassigned_meas: 
@@ -134,14 +123,11 @@ class Trackmanagement:
         self.track_list.remove(track)
         
     def handle_updated_track(self, track):      
-        ############
-        # TODO Step 2: implement track management for updated tracks:
+        # Step 2 - implement track management for updated tracks:
         # - increase track score
+        track.score += 1./params.window
         # - set track state to 'tentative' or 'confirmed'
-        ############
-
-        pass
-        
-        ############
-        # END student code
-        ############ 
+        if track.score > params.delete_threshold and track.score < params.confirmed_threshold:
+            track.state = 'tentative'
+        if track.score >= params.confirmed_threshold and track.state == 'tentative':
+            track.state = 'confirmed'
